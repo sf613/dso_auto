f27efbb12460accd309a0aa2414a1a63a35501b6
Martin <mbrus85@gmail.com>
2014-08-02 08:17:05 +0300

    minor changes in class structure

---
+++ abstractmultibot.rb
@@ -0,0 +1,109 @@
+require 'abstractbot'
+
+class AbstractMultibot < AbstractBot
+  attr_accessor :sikuli_executor,:screen, :sikuli, :image_path,:variant
+  def initialize(variant)
+    end
+
+  end
+
+  def buff_main_bakeries
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_all_bakeries
+  end
+
+  def buff_main_bows
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_all_bows
+  end
+
+  def buff_main_bronzeswords
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_all_bronzeswords
+  end
+
+  def buff_main_copper_smelters
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_all_copper_smelters
+  end
+
+  def buff_main_goldtowers
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_all_goldtowers
+  end
+
+  def buff_main_goldmines
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_all_goldmines
+  end
+
+  def buff_main_goldsmelters
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_all_goldsmelters
+  end
+
+  def buff_main_goldsmelters_min
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_min_goldsmelters
+  end
+
+  def buff_main_coinmakers
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_all_coinmakers
+  end
+
+  def buff_main_coinmakers_min
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_min_coinmakers
+  end
+
+  def buff_main_toolmakers
+    if @variant == "home"
+      @csv_path = @image_path+"/xyHome/main"
+    elsif @variant == "work"
+      @csv_path = @image_path+"/xyWork/main"
+    end
+    buff_all_toolmakers
+  end
+end#of class
+
--- multi2bot.rb
+++ multi2bot.rb
@@ -1,6 +1,6 @@
-require 'abstractbot'
+require 'abstractmultibot'
 
-class Multi2bot < AbstractBot
+class Multi2bot < AbstractMultiBot
 	attr_accessor :sikuli_executor,:screen, :sikuli, :image_path,:variant
 	def initialize(variant)
 		@browser = "Safari"
@@ -31,104 +31,6 @@ class Multi2bot < AbstractBot
 
 	end
 
-	def buff_main_bakeries
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_all_bakeries
-	end
-
-	def buff_main_bows
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_all_bows
-	end
-
-	def buff_main_bronzeswords
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_all_bronzeswords
-	end
-
-	def buff_main_copper_smelters
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_all_copper_smelters
-	end
-
-	def buff_main_goldtowers
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_all_goldtowers
-	end
-
-	def buff_main_goldmines
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_all_goldmines
-	end
-
-	def buff_main_goldsmelters
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_all_goldsmelters
-	end
-
-	def buff_main_goldsmelters_min
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_min_goldsmelters
-	end
-
-	def buff_main_coinmakers
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_all_coinmakers
-	end
-
-	def buff_main_coinmakers_min
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_min_coinmakers
-	end
-
-	def buff_main_toolmakers
-		if @variant == "home"
-			@csv_path = @image_path+"/xyHome/main"
-		elsif @variant == "work"
-			@csv_path = @image_path+"/xyWork/main"
-		end
-		buff_all_toolmakers
-	end
 end
 
 instance = Multi2bot.new("work")
